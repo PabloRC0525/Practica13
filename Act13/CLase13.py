dc25e9c0d826dd563b2173bfb5658d627ca9aa11
PabloRC0525 <121038827@upq.edu.mx>
2023-03-08 15:41:36 -0600

    Update CLase13.py

--- Act13/CLase13.py
+++ Act13/CLase13.py
@@ -1,4 +1,3 @@
-import tkinter as tk
 import string
 import random
 from tkinter import messagebox
@@ -15,18 +14,16 @@ class GenerarContraseña:
         Mayus = self.may.get()
         especial = self.esp.get()
         
-        # Creamos una lista de caracteres posibles para la contraseña
         caracteres = string.ascii_lowercase
         if Mayus:
             caracteres += string.ascii_uppercase
         if especial:
             caracteres += string.punctuation
         
-        # Generamos la contraseña aleatoria
         password = ''.join(random.choice(caracteres) for i in range(largo))
         self.contr.set(password)
         messagebox.showinfo("Contraseña","La contraseña generada es: " + password)
-        # Verificamos la fortaleza de la contraseña
+        
         score = 0
         if any(c.isupper() for c in password):
             score += 1
